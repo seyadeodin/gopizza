52eb24ec8902deb6401d695029ac190ca7b47272
e-lucas-gestao <89854299+e-lucas-gestao@users.noreply.github.com>
2022-01-21 15:49:54 -0300

    add product screen finished

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,13 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Platform, TouchableOpacity } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 import { ButtonBack } from '@components/ButtonBack';
+import { Photo } from '@components/Photo';
+import { InputPrice } from '@components/InputPrice';
+import { Input } from '@components/Input';
+import { Button } from '@components/Button';
 
-import { Container, Header, Title, DeleteLabel } from './styles';
+import { 
+  Container, 
+  Header, 
+  Title, 
+  DeleteLabel,
+  Upload,
+  PickImageButton,
+  Form,
+  Label,
+  InputGroup,
+  InputGroupHeader,
+  MaxCharacters,
+} from './styles';
+import { ScrollView } from 'react-native-gesture-handler';
 
 export function Product() {
+  const [ image, setImage ] = useState('');
+
+  async function handlePickerImage(){
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if(status === 'granted') {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        aspect: [4, 4]
+      });
+
+      if(!result.cancelled){
+        setImage(result.uri)
+      }
+    }
+  }
+
   return(
     <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+      <ScrollView showsHorizontalScrollIndicator={false} >
+
       <Header>
         <ButtonBack/>
         <Title>Cadastrar</Title>
@@ -15,8 +52,51 @@ export function Product() {
           <DeleteLabel>Deletar</DeleteLabel>
         </TouchableOpacity>
       </Header>
+
+      <Upload>
+        <Photo uri={image} />
+
+        <PickImageButton 
+          title="Carregar" 
+          type="secondary"
+          onPress={handlePickerImage}
+        />
+      </Upload>
+
+      <Form>
+        <InputGroup>
+          <Label>Nome</Label>
+          <Input/>
+        </InputGroup>
+
+        <InputGroup>
+          <InputGroupHeader>
+            <Label>Descrição</Label>
+            <MaxCharacters>0 de 60 caracteres</MaxCharacters>
+          </InputGroupHeader>
+          <Input
+            multiline
+            maxLength={60}
+            style={{ height: 80}}
+          />
+        </InputGroup>
+
+
+        <InputGroup>
+          <Label>Tamanhos e preços</Label>
+
+          <InputPrice size="P" />
+          <InputPrice size="M" />
+          <InputPrice size="G" />
+        </InputGroup>
+
+        <Button
+          title="Cadastrar pizza"
+        />
+      </Form>
      
 
+      </ScrollView>
     </Container>
   )
 }
--- src/screens/Product/styles.ts
+++ src/screens/Product/styles.ts
@@ -1,6 +1,7 @@
 import { LinearGradient } from 'expo-linear-gradient';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 import styled, { css } from 'styled-components/native';
+import { Button } from '@components/Button'
 
 export const Container = styled.KeyboardAvoidingView`
   flex: 1;
@@ -35,4 +36,52 @@ export const  DeleteLabel = styled.Text`
     color: ${theme.COLORS.TITLE}
   `}
 
+`
+
+export const Upload = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin: 32px 0;
+`;
+
+export const PickImageButton = styled(Button)`
+  max-width: 90px;
+  margin-left: 32px;
+`;
+
+export const Form = styled.View`
+  width: 100%;
+  padding: 24px;
+`
+
+export const Label = styled.Text`
+  margin-bottom: 12px;
+  font-size: 14px;
+
+  ${({theme}) => css `
+    font-family: ${theme.FONTS.TEXT};
+    color: ${theme.COLORS.SECONDARY_900};
+  `};
+`
+export const InputGroup = styled.View`
+  width: 100%;
+  margin-bottom: 16px;
+`
+export const InputGroupHeader = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const MaxCharacters = styled.Text`
+  font-size: 10px;
+  margin-bottom: 12px;
+
+  ${({theme}) => css `
+    font-family: ${theme.FONTS.TEXT};
+    color: ${theme.COLORS.SECONDARY_900};
+  `}
 `
