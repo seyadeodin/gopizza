369a82e597f545e4eff132be29ab3c6be207a7c0
e-lucas-gestao <89854299+e-lucas-gestao@users.noreply.github.com>
2022-01-25 12:18:42 -0300

    homepage finisheD

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Alert, TouchableOpacity, FlatList } from 'react-native';
 import { useTheme } from 'styled-components/native'
 import { MaterialIcons } from '@expo/vector-icons'
+import firestore from '@react-native-firebase/firestore'
 
 import happyEmoji from '@assets/happy.png'
 
 import { Search } from '@components/Search'
+import { ProductCard, ProductProps } from '@components/ProductCard'
 
 import { 
   Container, 
@@ -13,11 +15,52 @@ import {
   Greeting,
   GreetingEmoji,
   GreetingText,
+  MenuHeader,
+  MenuItemsNumber,
+  Title,
 } from './styles';
-import { Alert } from 'react-native';
 
 export function Home(){
+  const [ pizzas, setPizzas ] = useState<ProductProps[]>([])
+  const [ search, setSearch ] = useState('')
   const { COLORS } = useTheme();
+
+  function fetchPizzas(value: string) {
+    const formattedValue = value.toLowerCase().trim();
+
+    firestore()
+    .collection('pizzas')
+    .orderBy('name_insensitive')
+    .startAt(formattedValue)
+    .endAt(`${formattedValue}\uf8ff`)
+    //The \uf8ff character used in the query above is a very high code point in the Unicode range. Because it is after most regular characters in Unicode, the query matches all values that start with a term.
+    .get()
+    .then(response => {
+      console.log(response)
+      const data = response.docs.map(doc => {
+        return {
+          id: doc.id,
+          ...doc.data(),
+        }
+      }) as ProductProps[];
+
+      setPizzas(data)
+    })
+    .catch(() => Alert.alert('Consulta', 'Não foi possível realizar a consulta'));
+  }
+
+  function handleSearch(){
+    fetchPizzas(search)
+  }
+
+  function handleSearchClear(){
+    setSearch('')
+    fetchPizzas('')
+  }
+
+  useEffect(() => {
+    fetchPizzas('')
+  }, [])
 
   return (
     <Container>
@@ -30,11 +73,32 @@ export function Home(){
         <TouchableOpacity onPress={() => Alert.alert('Teste')}>
           <MaterialIcons name="logout" color={COLORS.TITLE} size={24} />
         </TouchableOpacity>
-
-
       </Header>
-        <Search onSearch={() => {}} onClear={() => {}} />
 
+      <Search 
+        value={search}
+        onChangeText={setSearch}
+        onSearch={handleSearch}
+        onClear={handleSearchClear} 
+      />
+
+      <MenuHeader>
+        <Title>Cardápio</Title>
+        <MenuItemsNumber>10 pizzas</MenuItemsNumber>
+      </MenuHeader>
+
+      <FlatList
+        data={pizzas}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => <ProductCard data={item} />}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle = {{
+          paddingTop: 20,
+          paddingBottom: 125,
+          marginHorizontal: 24,
+        }}
+      />
+        
     </Container>
   )
 }
